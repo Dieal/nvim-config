d2966882b018736f0507a1ec532648a2e96e9a14
dieal <59124097+Dieal@users.noreply.github.com>
2023-08-25 20:58:59 +0200

    Add split windows navigation keymaps

--- after/plugin/navigation.lua
+++ after/plugin/navigation.lua
@@ -5,7 +5,7 @@ local ui = require('harpoon.ui')
 vim.keymap.set('n', '<leader>hm', mark.add_file, { desc = '[H]arpoon: [M]ark file' });
 vim.keymap.set('n', '<leader>hr', mark.rm_file, { desc = '[H]arpoon: [R]emove Mark' });
 vim.keymap.set('n', '<leader>ht', ui.toggle_quick_menu, { desc = '[H]arpoon: [T]oggle Ui' });
-vim.keymap.set('n', '<C-j>', ui.nav_next, { desc = 'Harpoon: Next Mark' });
+vim.keymap.set('n', '<C-n>', ui.nav_next, { desc = 'Harpoon: next mark' });
 
 -- Disable Netwr
 vim.g.loaded_netrw       = 1
--- lua/dieal/keymaps.lua
+++ lua/dieal/keymaps.lua
@@ -15,6 +15,7 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, 'Q', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -29,3 +30,9 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- Floating terminal
 vim.keymap.set('n', '<A-t>', '<CMD>lua require("FTerm").toggle()<CR>', { desc = '[T]oggle Terminal' })
 vim.keymap.set('t', '<A-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { desc = '[T]oggle Terminal' })
+
+-- Split windows navigation
+vim.keymap.set('n', '<C-l>', '<CMD>wincmd l<CR>', { desc = 'Go to right split window' })
+vim.keymap.set('n', '<C-h>', '<CMD>wincmd h<CR>', { desc = 'Go to left split window' })
+vim.keymap.set('n', '<C-j>', '<CMD>wincmd j<CR>', { desc = 'Go to lower split window' })
+vim.keymap.set('n', '<C-k>', '<CMD>wincmd k<CR>', { desc = 'Go to higher split window' })
